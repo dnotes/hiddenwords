f3353c22eaa9d757efc367b499e7929857fda539
David Hunt <git@dnotes.net>
2019-12-28 10:35:35 -0800

    better ux on the navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -13,17 +13,19 @@
 <style>
 	div.menu {
 		font-weight: 300;
-		line-height: 56px;
+		line-height: 30px;
 		position: fixed;
 		top: 0;
 		right: 0;
-		width: 180px;
-		height: 56px;
-		overflow: hidden;
+		width: 100%;
+		height: 46px;
+		padding-left: 6px;
+		box-shadow: 0 3px 2px rgba(126,126,126,.6);
 		z-index: 10;
 	}
 
-	div.menu.showSettings { height: auto; }
+	div.settings { display:none; width:180px; padding-top:19px; border-bottom-right-radius:8px; box-shadow: 3px 3px 2px rgba(126,126,126,.6)}
+	div.settings.showSettings { display:block; }
 
 	.row {
 		text-align: center;
@@ -50,8 +52,8 @@
 		text-decoration: none;
 		display: inline-block;
 		width: 60px;
-		height: 60px;
-		line-height: 60px;
+		height: 46px;
+		line-height: 46px;
 		text-align: center;
 	}
 	a.selected { color: aliceblue; }
@@ -69,26 +71,28 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </svelte:head>
 
-<div class="menu" class:showSettings on:mouseOut="{showSettings=false}">
+<div class="menu" class:showSettings>
 	<ul>
 		<li><a title="home" class:selected='{segment === undefined}' href='/'><i class="fa fa-home" /></a></li>
 		<li><a title="read" rel=prefetch class:selected='{segment === "hw"}' href="/hw"><i class="fa fa-book" /></a></li> 
 		<li><a title="settings" class:selected='{segment === "settings"}' href="/settings" on:click|preventDefault={toggleSettings}><i class="fa fa-cog" /></a></li>
 	</ul>
-	<div class="row">
-		<div>
-			<SettingButton setting={linkDisplay} value="grid"><i class="fa fa-th" /></SettingButton>
-			<SettingButton setting={linkDisplay} value="list"><i class="fa fa-list" /></SettingButton>
+	<div class="settings" class:showSettings on:mouseOut="{() => {showSettings=false}}">
+		<div class="row">
+			<div>
+				<SettingButton setting={linkDisplay} value="grid"><i class="fa fa-th" /></SettingButton>
+				<SettingButton setting={linkDisplay} value="list"><i class="fa fa-list" /></SettingButton>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div>
-			{#each themes as i}
-				<SettingButton setting={theme} value="{i.id}"><ThemeSetting {...i} /></SettingButton>
-			{/each}
+		<div class="row">
+			<div>
+				{#each themes as i}
+					<SettingButton setting={theme} value="{i.id}"><ThemeSetting {...i} /></SettingButton>
+				{/each}
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<a href="/about">about</a>
+		<div class="row">
+			<a href="/about">about</a>
+		</div>
 	</div>
 </div>
