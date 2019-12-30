623ed0618ee18d3cbddd4af3b43fda7df22d789e
David Hunt <git@dnotes.net>
2019-12-29 21:47:10 -0800

    Fixed external links in readme/about

--- src/routes/about.json.js
+++ src/routes/about.json.js
@@ -7,7 +7,7 @@ let html
 
 export function get(req, res) {
 	if (!html || process.env.NODE_ENV !== 'production') {
-		html = md.render(fs.readFileSync(path.resolve('README.md')).toString())
+		html = md.render(fs.readFileSync(path.resolve('README.md')).toString()).replace(/<a /gm, '<a target="_blank" ')
 	}
   res.writeHead(200, {
     'Content-Type': 'application/json'
