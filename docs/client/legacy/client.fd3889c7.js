d23f8530107ca2b7a10db74ce02ecfb5e1a29708
David Hunt <git@dnotes.net>
2019-12-29 23:53:57 -0800

    trying sapper export --legacy for github pages?

---
+++ docs/client/legacy/client.fd3889c7.js
@@ -0,0 +1,1 @@
+import"./index.bedc7149.js";import"./slicedToArray.f13a096f.js";import"./asyncToGenerator.a01952a5.js";import{s as r}from"./app.960e609b.js";import"./store.52846353.js";r({target:document.querySelector("#sapper")});
