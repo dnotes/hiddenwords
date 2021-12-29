5914e2840d56f3f63e160da795c21300d7a78c7d
David Hunt <git@dnotes.net>
2021-12-29 01:33:01 -0800

    add momentary highlight to current hw in list

--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
-import { grid, zen } from '$lib/stores'
+import { grid, zen, current } from '$lib/stores'
   export let hw
   export let narrow = false
   export let searchtext = ''
   const numbered = hw.number ? true : false
+
   $: href = $zen ? `/${hw.id}` : `/#${hw.id.replace('/','-')}`
   $: found = !searchtext || hw.searchtext.includes(searchtext)
+
+  function click() { $current=hw }
+
 </script>
 
 {#if $grid}
-  <a on:click class="inline-block grid-item" class:opacity-10={!found} class:narrow class:numbered {href} title="{hw.teaser}">{hw.number || hw.title}</a>
+  <a on:click={click} class="inline-block grid-item" class:opacity-10={!found} class:narrow class:numbered {href} title="{hw.teaser}">{hw.number || hw.title}</a>
 {:else}
-  <a on:click class="block" class:hidden={!found} {href}>{hw.number || hw.title}: “{hw.teaser}”</a>
+  <a on:click={click} class="block" class:hidden={!found} {href}>{hw.number || hw.title}: “{hw.teaser}”</a>
 {/if}
 
 <style lang="postcss">
--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -1,21 +1,60 @@
 <script lang="ts">
-  import { brightness } from '$lib/stores'
+  import { brightness, current } from '$lib/stores'
+  import { quadIn, quadOut, quadInOut } from 'svelte/easing'
   export let items
+
+  function colorize(node, { duration }) {
+    const color = getComputedStyle(node).backgroundColor
+    const end = (color.match(/\d+/) || [])[0] > "200" ? 0 : 255
+
+    return {
+      duration,
+      easing: quadOut,
+      css: t => {
+        return `color:rgb(${218 + ((end-218)*t)}, ${200 + ((end-200)*t)}, ${45 + ((end-45)*t)})`
+      }
+    }
+  }
 </script>
 
-{#each items as { id,title,section,number,exhort,text,teaser,pretext,anchor } }
-  <div class="mt-6">
-    {#if pretext}
-      <p class="font-script text-sm my-4">{pretext}</p>
+<div class="max-w-screen-xl mx-auto lg:text-xl xl:text-2xl">
+  {#each items as hw}
+    {#if $current?.['id'] === hw.id}
+      <div class="mt-6 bg-white dark:bg-gray-900" in:colorize="{{duration:800}}">
+        {#if hw.pretext}
+          <p class="font-script text-sm my-4">{hw.pretext}</p>
+        {/if}
+        <p>
+          <a class="anchor" id="{hw.anchor}">{hw.number || hw.title}</a>
+          {#if hw.number}
+            <a class="block float-left w-4 -ml-6 text-center opacity-{$brightness}" href="/#{hw.anchor}" on:click={()=>{$current=hw}}>{hw.number}</a>
+          {/if}
+          {#if hw.exhort}
+            <span class="font-exhort mb-2 inline-block">{hw.exhort}</span><br>
+          {/if}
+          {hw.text}
+        </p>
+      </div>
+    {:else}
+      <div class="mt-6">
+        {#if hw.pretext}
+          <p class="font-script text-sm my-4">{hw.pretext}</p>
+        {/if}
+        <p>
+          <a class="anchor" id="{hw.anchor}">{hw.number || hw.title}</a>
+          {#if hw.number}
+            <a class="block float-left w-4 -ml-6 text-center opacity-{$brightness}" href="/#{hw.anchor}" on:click={()=>{$current=hw}}>{hw.number}</a>
+          {/if}
+          {#if hw.exhort}
+            <span class="font-exhort mb-2 inline-block">{hw.exhort}</span><br>
+          {/if}
+          {hw.text}
+        </p>
+      </div>
     {/if}
-    <p>
-      {#if number}
-        <a class="block float-left w-4 -ml-6 text-center opacity-{$brightness}" href="/{anchor}" id={anchor}>{number}</a>
-      {/if}
-      {#if exhort}
-        <span class="font-exhort mb-2 inline-block">{exhort}</span><br>
-      {/if}
-      {text}
-    </p>
-  </div>
-{/each}
+  {/each}
+</div>
+
+<style>
+  a.anchor { @apply block relative -top-40; visibility:hidden; color:rgb(218, 200, 45) }
+</style>
