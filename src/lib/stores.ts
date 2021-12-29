274ddafbc568650efa0bd739c8cac8ce82ee779b
David Hunt <git@dnotes.net>
2021-12-28 20:52:55 -0800

    don't save showSettings / showContents

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,5 @@
 import { writable } from 'svelte-local-storage-store'
+import { writable as writeableNoStore } from 'svelte/store'
 
 export const grid = writable('grid', false)
 export const zen = writable('zen', false)
@@ -7,7 +8,7 @@ export const reading = writable('reading', true)
 export const autoscroll = writable('autoscroll', true)
 export const brightness = writable('brightness', 50)
 
-export const showSettings = writable('showSettings', false)
-export const showContents = writable('showContents', false)
+export const showSettings = writeableNoStore(false)
+export const showContents = writeableNoStore(false)
 
 export const current = writable('current', {})
