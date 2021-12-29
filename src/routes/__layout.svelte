45cebe98fc6b2154dbfb519fbccb87a6a208ffb7
David Hunt <git@dnotes.net>
2021-12-28 21:46:12 -0800

    fixed footer should scroll over text

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
   <div class="pb-20 px-4 sm:px-9 md:px-16 h-full overflow-y-scroll">
     <slot />
   </div>
-  <div class="fixed bottom-0 left-0 w-full h-10 sm:bg-white sm:dark:bg-gray-900">
+  <div class="fixed bottom-0 left-0 w-full h-10 sm:bg-white sm:dark:bg-gray-900 z-50">
     <div class="w-full bottom-0 absolute">
       <Nav />
     </div>
