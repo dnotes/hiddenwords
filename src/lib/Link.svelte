337d341e5cc43fccfedd715c709fd3c1a525baf6
David Hunt <git@dnotes.net>
2022-01-07 04:18:58 -0800

    fixed spacing on TOC links

--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -8,25 +8,28 @@ import { grid, zen, current } from '$lib/stores'
   $: href = $zen ? `/${hw.id}` : `/#${hw.id.replace('/','-')}`
   $: found = !searchtext || hw.searchtext.includes(searchtext)
 
+
+
   function click() { $current=hw }
 
 </script>
 
 {#if $grid}
-  <a on:click={click} class="inline-block grid-item" class:opacity-10={!found} class:narrow class:numbered {href} title="{hw.teaser}">{hw.number || hw.title}</a>
+  <div class="basis-[10%] flex-grow-0 {narrow ? 'p-px' : 'p-1'}">
+    <a on:click={click} class="grid-item" class:opacity-10={!found} class:narrow class:numbered {href} title="{hw.teaser}">{hw.number || hw.title}</a>
+  </div>
 {:else}
   <a on:click={click} class="block" class:hidden={!found} {href}>{hw.number || hw.title}: “{hw.teaser}”</a>
 {/if}
 
 <style lang="postcss">
   a.grid-item {
-    @apply px-4 py-2 rounded-lg border-2 border-blue-500 flex-grow-0 text-center m-1;
+    @apply inline-block px-4 py-2 rounded-lg border-2 border-blue-500 text-center;
   }
   a.grid-item.narrow {
-    @apply border m-px px-1;
+    @apply border px-1;
   }
   a.grid-item.numbered {
-    @apply px-0;
-    width:calc(10% - .5rem);
+    @apply px-0 w-full;
   }
 </style>
