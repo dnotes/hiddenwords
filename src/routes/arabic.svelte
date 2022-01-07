1b16c1fedcaad0416939e010c05645fdcce1ce6f
David Hunt <git@dnotes.net>
2022-01-07 09:32:16 -0800

    added /arabic and /persian routes (redirects)

--- src/routes/arabic.svelte
+++ src/routes/arabic.svelte
@@ -1,25 +1,11 @@
-<script context="module" lang="ts">
-  export async function load({fetch}) {
-    try {
-      const res = await fetch('/hw.json')
-      if (!res.ok) {
-        return {
-          status: res.status,
-          error: res.message,
-        }
-      }
-      let items = await res.json()
-      items = items.slice(0,73)
-      return {
-        props: {
-          items
-        }
-      }
-    }
-    catch(e) {
-      return {
-        error: e
-      }
-    }
+<script lang="ts">
+  import { paused, zen } from '$lib/stores'
+  import { goto } from '$app/navigation'
+  import { browser } from '$app/env'
+
+  if (browser) {
+    $zen = false
+    $paused = true
+    goto('/#arabic')
   }
 </script>
---
+++ src/routes/persian.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { paused, zen } from '$lib/stores'
+  import { goto } from '$app/navigation'
+  import { browser } from '$app/env'
+
+  if (browser) {
+    $zen = false
+    $paused = true
+    goto('/#persian')
+  }
+</script>
