9cb0851dc0e4b6980a80a645ec3f677379968688
David Hunt <git@dnotes.net>
2022-01-20 13:56:55 -0800

    added CurrentLink / close button for about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,11 +9,14 @@
 
 <script>
 import Sitelinks from '$lib/Sitelinks.svelte';
+import CurrentLink from '$lib/CurrentLink.svelte'
   export let item
 </script>
 
+<CurrentLink />
+
 <div class="mx-auto w-3/5 max-w-md bg-contain bg-logo dark:bg-logo-dark">
-  <img class="opacity-0 w-full" alt="the inner essence" src="/inneressence.png" />
+  <img class="opacity-0 w-full" alt="The Inner Essence : The Hidden Words of Bahá’u’lláh" src="/inneressence.png" />
 </div>
 
 <!-- about page -->
